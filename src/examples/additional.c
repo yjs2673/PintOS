64cfb4eac227c4f9ae8f3f277156d046506a210d
yjs2673 <yjs2673@naver.com>
2025-09-19 11:43:29 +0000

    [FEAT] 기능 구현

--- src/examples/additional.c
+++ src/examples/additional.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "user/syscall.h"
 
-int main()
+int
+main (int argc, char *argv[])
 {
-    return 0;
+  if (argc != 4) {
+    printf("usage: additional <n> <a> <b> <c> <d>\n");
+    return EXIT_FAILURE;
+  }
+
+  int a = atoi(argv[1]);
+  int b = atoi(argv[2]);
+  int c = atoi(argv[3]);
+  int d = atoi(argv[4]);
+
+  int fib = fibonacci(a);
+  int mx  = max_of_four_int(a, b, c, d);
+
+  printf("%d %d\n", fib, mx);
+  return EXIT_SUCCESS;
 }
