7deb4bc58320d175751638ac5e2fdb7940017780
yjs2673 <yjs2673@naver.com>
2025-09-19 11:59:13 +0000

    [MOD] 로직 수정

--- src/examples/additional.c
+++ src/examples/additional.c
@@ -2,22 +2,20 @@
 #include <stdlib.h>
 #include "user/syscall.h"
 
-int
-main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-  if (argc != 4) {
-    printf("usage: additional <n> <a> <b> <c> <d>\n");
-    return EXIT_FAILURE;
+  if (argc == 2) {
+    int a = atoi(argv[1]);
+    printf("%d\n", fibonacci(a));
+  }
+  else if (argc == 5)
+  {
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    int c = atoi(argv[3]);
+    int d = atoi(argv[4]);
+    printf("%d %d\n", fibonacci(a), max_of_four_int(a, b, c, d));
   }
 
-  int a = atoi(argv[1]);
-  int b = atoi(argv[2]);
-  int c = atoi(argv[3]);
-  int d = atoi(argv[4]);
-
-  int fib = fibonacci(a);
-  int mx  = max_of_four_int(a, b, c, d);
-
-  printf("%d %d\n", fib, mx);
   return EXIT_SUCCESS;
 }
