7e497ec09956987b20c016b34a653d813925bd95
yjs2673 <yjs2673@naver.com>
2025-09-30 07:01:49 +0000

    [ADD] upload user porgram 1

---
+++ os_prj1_20211558/src/examples/additional.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "user/syscall.h"
+
+int main (int argc, char *argv[])
+{
+  if (argc != 2 && argc != 5) 
+  {
+    printf("too less or large arguments to execute additional!\n");
+    return EXIT_SUCCESS;
+  }
+
+  if (argc == 2) 
+  {
+    int a = atoi(argv[1]);
+    printf("%d\n", fibonacci(a));
+    return EXIT_SUCCESS;
+  }
+
+  int a = atoi(argv[1]);
+  int b = atoi(argv[2]);
+  int c = atoi(argv[3]);
+  int d = atoi(argv[4]);
+  printf("%d %d\n", fibonacci(a), max_of_four_int(a, b, c, d));
+
+  return EXIT_SUCCESS;
+}
---
+++ os_prj1_20211558/src/userprog/syscall.h
@@ -0,0 +1,10 @@
+#ifndef USERPROG_SYSCALL_H
+#define USERPROG_SYSCALL_H
+
+#include <debug.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+void syscall_init (void);
+
+#endif /* userprog/syscall.h */
