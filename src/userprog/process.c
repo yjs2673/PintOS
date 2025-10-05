db90edfa1216650d8cb459bdf926eb7d32cf5738
yjs2673 <yjs2673@naver.com>
2025-10-05 17:37:17 +0000

    파일 접근 권한 로직 추가

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -141,6 +141,12 @@ process_exit (void)
   struct thread *cur = thread_current ();
   uint32_t *pd;
 
+  if (cur->exec_file != NULL)
+  {
+    file_allow_write(cur->exec_file);
+    file_close(cur->exec_file);
+  }
+
   /* Destroy the current process's page directory and switch back
      to the kernel-only page directory. */
   pd = cur->pagedir;
@@ -293,15 +299,17 @@ load (const char *file_name, void (**eip) (void), void **esp)
   }
 
   /* Open executable file. */
-  file = filesys_open (argv[0]);
-  if (file == NULL) 
-    {
-      printf ("load: %s: open failed\n", file_name);
-      goto done; 
-    }
+  t->exec_file = filesys_open (argv[0]);
+  if (t->exec_file == NULL) 
+  {
+    printf ("load: %s: open failed\n", file_name);
+    goto done; 
+  }
+
+  file_deny_write(t->exec_file); /* 실행 파일에 대한 쓰기 금지 */
 
   /* Read and verify executable header. */
-  if (file_read (file, &ehdr, sizeof ehdr) != sizeof ehdr
+  if (file_read (t->exec_file, &ehdr, sizeof ehdr) != sizeof ehdr
       || memcmp (ehdr.e_ident, "\177ELF\1\1\1", 7)
       || ehdr.e_type != 2
       || ehdr.e_machine != 3
@@ -319,11 +327,11 @@ load (const char *file_name, void (**eip) (void), void **esp)
     {
       struct Elf32_Phdr phdr;
 
-      if (file_ofs < 0 || file_ofs > file_length (file))
+      if (file_ofs < 0 || file_ofs > file_length (t->exec_file))
         goto done;
-      file_seek (file, file_ofs);
+      file_seek (t->exec_file, file_ofs);
 
-      if (file_read (file, &phdr, sizeof phdr) != sizeof phdr)
+      if (file_read (t->exec_file, &phdr, sizeof phdr) != sizeof phdr)
         goto done;
       file_ofs += sizeof phdr;
       switch (phdr.p_type) 
@@ -340,7 +348,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
         case PT_SHLIB:
           goto done;
         case PT_LOAD:
-          if (validate_segment (&phdr, file)) 
+          if (validate_segment (&phdr, t->exec_file)) 
             {
               bool writable = (phdr.p_flags & PF_W) != 0;
               uint32_t file_page = phdr.p_offset & ~PGMASK;
@@ -362,7 +370,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
                   read_bytes = 0;
                   zero_bytes = ROUND_UP (page_offset + phdr.p_memsz, PGSIZE);
                 }
-              if (!load_segment (file, file_page, (void *) mem_page,
+              if (!load_segment (t->exec_file, file_page, (void *) mem_page,
                                  read_bytes, zero_bytes, writable))
                 goto done;
             }
@@ -425,7 +433,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
 
  done:
   /* We arrive here whether the load is successful or not. */
-  file_close (file);
+  // file_close (t->exec_file);
   return success;
 }
 
